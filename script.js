253b113ffff4384b7594534ad611170cdc0d8e43
Dixita Darji <69450520+DixitaDarji@users.noreply.github.com>
2020-09-15 13:47:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function checkDriverAge(age){
 	
 	if(Number(age)<18){
-		return "Sorry, you are too yound to drive this car. Powering off";
 	}
 	else if(Number(age)>18){
 		return "Powering On. Enjoy the ride!";
